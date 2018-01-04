cc82de0f4e5e891f1f9857d757b44dbd8b77d520
Lidian Vilazio <lidianvilazio@hotmail.com>
2018-01-04 23:38:48 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,9 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 
-row = ["   ", "|   |", "   "]
+row = ["   ", "|   |", "-----------"]
 def display_board
-  puts
+  puts row[0] row[1] row[0]
+  puts row[2]
+  puts row[0] row[1] row[0]
+  puts row[2]
 end
